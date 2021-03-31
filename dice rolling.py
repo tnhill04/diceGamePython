1df807dc6c3a2d083238fe804b263157e3b81df3
Tyler Hill <Tyler@glarmsi.local>
2021-03-31 10:36:32 -0400

    added notes

--- dice rolling.py
+++ dice rolling.py
@@ -1,14 +1,8 @@
 import random
-from datetime import datetime
-
-# today = datetime.date.today()
-# future = datetime.date(2021, 9, 20)
-# diff = future - today
-# print(diff.days)
-
-#------------------------------------------------------------------
+import datetime
 
 
+#This function generates six random numbers
 def random_number(num):
     list_random = list()
     while num < 6:
@@ -17,6 +11,7 @@ def random_number(num):
         print(list_random)
     return list_random
 
+# This function adds the six numbers together
 def sum_up(list_sum):
     total = 0
     for item in list_sum:
@@ -24,41 +19,42 @@ def sum_up(list_sum):
         #total = toal + item Another way to do line 103
     return total
 
+# This function takes the player's name
 def get_name():
     name = input("Enter your name: ")
     return name
 
+# This compares the scores to tell who won
 def compare_scores(p1, p2):
     if player1 > player2:
         print("Player 1 wins")
     else:
         print("Player 2 wins")
 
+# This function is a countdown til Easter
 def time_stamp():
     today = datetime.date.today()
     future = datetime.date(2021, 4, 4)
     diff = future - today
-    print(diff.days)
-    # return diff.days
+    print(diff.days, "days til Easter! Happy Easter and Happy Coding")
+    return diff.days
 
-    # game_start = datetime.today().strftime('%b-%d-%Y')
-    # print(game_start)
-    # return game_start
-
-
+# This is the dictionary where the "name" and sum of the random numbers are stored
 mydict = dict()
 
 
+# This For loop puts the "name" and total of the six random numbers in a dictionary
 for x in range(2):
     name = get_name()
     mydict[name] = sum_up(random_number(0))
 
-
+# This pulls the values of dictionary
 values = mydict.values()
 
+# This pulls the values of mydict.values into a list
 values_list = list(values)
 
-
+# This stores the first player's value in
 player1 = values_list[0]
 
 player2 = values_list[1]
@@ -66,7 +62,6 @@ player2 = values_list[1]
 
 
 print(mydict)
-print(values_list)
 compare_scores(player1, player2)
 time_stamp()
 
--- test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-
-today = datetime.date.today()
-future = datetime.date(2021, 4, 4)
-diff = future - today
-print(diff.days)
